ff2842d7e9ecb8ef71c202ea6dc41bc5f328ea62
G-yws233 <1091389174@qq.com>
2017-02-09 22:53:14 +0800

    six

--- WeChat 钱包/rem.js
+++
@@ -1,8 +0,0 @@
-var designWidth=1080;
-function resize() {
-    var Width=document.documentElement.clientWidth;
-    var Font=Width/designWidth*100;
-    document.documentElement.style.fontSize=Font+"px";
-}
-window.addEventListener("resize",resize);
-resize();
